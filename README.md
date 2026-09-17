e9c8e45c48155c3e35a27656f2cea71a5930a7bd
Григорий <xoppee.ru@yandex.com>
2026-09-16 22:07:41 -0300

    Things

---
+++ README.md
@@ -0,0 +1,12 @@
+## Fyn1x - Multi-Agent Orchestrator
+
+### Features
+
+<p>It shows a banner</p>
+
+
+### Future Features
+
+<p>AI Agents</p>
+<p>Orchestrator with animation</p>
+<p>And more...</p>
--- src/fyn1x/main.py
+++ src/fyn1x/main.py
@@ -7,6 +7,9 @@ from rich.console import Console
 from rich.panel import Panel
 from rich.prompt import Prompt
 from pyfiglet import Figlet
+from ddgs import DDGS
+from .parser import loads, dumps
+
 
 console = Console()
 
@@ -15,6 +18,21 @@ def showBanner():
     banner = figlet.renderText("FYN1X")
     console.print(f"[bold magenta]{banner}[/bold magenta]")
     console.print(f"[dim]Multi-Agent code reviewer[/dim]\n")
+
+'''
+"$q" is the variable which gets user prompt and the keywords of it but for now we'll just accept simple requests
+'''
+
+def search(q):
+    results = DDGS().text(q, max_results=2)
+    parsed = results
+    print("Parsed:")
+    print(parsed)
+
+
+    print("\nSerialized:")
+    print(dumps(parsed, indent=2))
+
 
 def main():
     showBanner()
@@ -26,6 +44,7 @@ def main():
             if user_input.lower() in ("exit", "quit", "q"):
                 break
             console.print(f"[green]You said:[/green] {user_input}\n")
+            search(user_input)
         except (KeyboardInterrupt, EOFError):
             break
     console.print("[dim]Goodbye.[/dim]")
